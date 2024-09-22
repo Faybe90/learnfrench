a09b46a9c2b13d617789865371739ed000d534bb
Faybe90 <fai.behlouli@gmail.com>
2024-09-22 20:31:22 +0100

    Update files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"; // Si tu veux appliquer du CSS global
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Bienvenue sur Apprendre le français !</h1>
+        <p>Voici quelques articles où je parlerai du programme.</p>
       </header>
+
+      <section className="images">
+        {/* Ajout des nouvelles images ici */}
+        <img src="/images/frenchlogo.png" alt="logo" />
+      </section>
+
+      <section className="articles">
+        <article>
+          <h2>1as</h2>
+          <p>
+            Le programme de 1 as comporte des projets divers : <br />
+            la vulgarisation scientifique <br />
+            l'argumentation <br />
+            le style journalistique et les interviews <br />
+            la narration
+          </p>
+          <img src="/images/books1.png" alt="1as" />
+        </article>
+
+        <article>
+          <h2>2as</h2>
+          <p>
+            Le programme de 2 as comporte des projets divers : <br />
+            l'objectivation scientifique <br />
+            l'argumentation comme le plaidoyer et le requisitoire <br />
+            le theatre <br />
+            la narration avec la nouvelle d'anticipation
+          </p>
+          <img src="/images/books2.png" alt="2as" />
+        </article>
+
+        <article>
+          <h2>3as</h2>
+          <p>
+            Le programme de 3 as comporte des projets divers : <br />
+            le texte d'Histoire et notamment la guerre d'Algerie <br />
+            l'argumentation comment convaincre et persuader <br />
+            lancer un appel ou comment pousser les lecteurs a agire <br />
+            la narration avec la nouvelle fantastique
+          </p>
+
+          <img src="/images/books3.png" alt="3as" />
+        </article>
+      </section>
     </div>
   );
 }
